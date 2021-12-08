fc83973e830c8e60ce07d4893e6242dc0feafa17
James Bradbury <me@jamesbradbury.net>
2021-12-08 20:58:07 +0000

    basic formatting and skeleton

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,28 +3,71 @@
 </script>
 
 <div class="container">
-    <div class="content">
-        <main>
-            <slot />
-        </main>
-    </div>
+	<!-- <Header /> -->
+	<!-- <Crumbs /> -->
+
+	<div class="content">
+		<!-- <div class="navigation"> -->
+			<!-- <TOC /> -->
+		<!-- </div> -->
+		<div class="main">
+			<slot />
+		</div>
+
+		<!-- <div class="empty-right" /> -->
+	</div>
+
+	<!-- <Footer /> -->
 </div>
     
-<style lang='scss'>
-    .container {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
 
-    .content {
-        display: grid;
-        justify-content: center;
-        margin: 1em;
-    }
+	.content {
+		display: grid;
+		justify-content: center;
+		margin: 1em;
+	}
 
-    main {
-        grid-area: main;
+	.main {
+		grid-area: main;
+		min-width: $min-text-width;
+		max-width: $max-text-width;
+	}
 
-    }
+	.navigation {
+		grid-area: navigation;
+		width: 20ch;
+		height: max-content;
+	}
+
+	.empty-right {
+		grid-area: empty-right;
+		width: 20ch;
+	}
+
+	/* Media Queries */
+	@media (min-width: $breakpoint) {
+		.content {
+			grid-template-columns: auto min($max-text-width, 100%) auto;
+			grid-template-areas: 'navigation main empty-right';
+		}
+	}
+
+	@media (max-width: $breakpoint) {
+		.content {
+			grid-template-rows: auto auto;
+			grid-template-areas:
+				'navigation'
+				'main';
+		}
+
+		.empty-right {
+			display: none;
+		}
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,16 @@
-<h1>coming soon...</h1>
+<h1>Civic Sound Week</h1>
+
+<p>
+    Civic Sound Week is a week-long series of performances, workshops and activities exploring sound art and music. Featuring some of the most interesting artists from around the world -  ranging from traditional Acholi fiddle, to improvisation, algorithmic music, live coding, DJ workshops, synthesizers, sound system culture, multi-speaker sound art, Sufi poetry, and Georgian polyphonic singing.  
+</p>
+
+<p>
+    Highlights include the renowned wildlife recordist Chris Watson, the huge Moog synthesizer sound lab, Huddersfield Immersive Sound System, and Sheffield based Sinai sound system. With performances from Oren Ambarchi, Keith Fullerton Whitman, Okkyung Lee, Rian Treanor, Ocen James, Ryoko Akama, Karl D’silva, and many more.
+</p>
+
+<p>
+    More information coming soon.
+</p>
 
 <style>
     h1 {
