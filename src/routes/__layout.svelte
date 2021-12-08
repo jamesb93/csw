bf224d00d13047196674c82ee182de847330b8fb
James Bradbury <me@jamesbradbury.net>
2021-12-08 11:28:20 +0000

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.scss';
+</script>
+
+<div class="container">
+    <div class="content">
+        <main>
+            <slot />
+        </main>
+    </div>
+</div>
+    
+<style lang='scss'>
+    .container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    .content {
+        display: grid;
+        justify-content: center;
+        margin: 1em;
+    }
+
+    main {
+        grid-area: main;
+
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<h1>coming soon...</h1>
+
+<style>
+    h1 {
+        color: grey;
+    }
+</style>
