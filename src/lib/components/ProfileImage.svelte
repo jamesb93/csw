3035c1c5896a586a191db54eef5399b7c4fd1a73
James Bradbury <me@jamesbradbury.net>
2022-01-16 15:10:31 +0000

    add photos

---
+++ src/lib/components/ProfileImage.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let src = '';
+    export let alt = '';
+</script>
+
+<img src={src} alt={alt}>
+
+<style>
+    img {
+        filter: grayscale();
+        -webkit-filter: grayscale();
+        max-width: 100%;
+        margin: 0 auto;
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+    import ProfileImage from '$lib/components/ProfileImage.svelte';
+</script>
+
 <p>Civic Sound Week</p>
 <p>Opening times 2pm to 8pm daily</p>
 
@@ -21,6 +25,8 @@
     The ocean is full of sounds - from the toothed whale's use of echo-location, the crackling sound minute creatures, to the melting of ten thousand year-old ice. Norwegian artist Jana Winderen’s Transmission uses underwater wildlife recordings, searching for barely audible and often inaccessible sounds in order to demonstrate their detail and complexity. Listen to fish, crustaceans, insect species and mammals which swim and move around you. With the spatial sound design developed by Tony Myatt. Winderen is an award winning artist whose work has been presented around the world.  
 </p>
 
+<ProfileImage src='/jana-min.jpg' alt='Jana Winderen profile photo'/>
+
 <p>~</p>
 
 <p>
@@ -33,6 +39,8 @@
 <p>
     Alex works in his handmade and free/open source TidalCycles environment, created for exploring algorithmic patterns. This work will show an array of eight of Alex's TidalCycles sessions, captured on loop, drifting out of sync, spread around the speakers in the space. Alex McLean is a musician and researcher based in Sheffield UK, and part of the independent, open access research studio Then Try This. 
 </p>
+
+<ProfileImage src='/alex-min.jpg' alt='Alex McClean profile photo'/>
 
 <p>
     Workshop 2pm
@@ -56,6 +64,8 @@
     Mark’s piece Protomusic#1 brings together recordings of 40 performers and instruments from various traditions to recreate environmental recordings made in the North of England, these range from bus stations, steel factories, archaeological digs and walks in the mountains. Mark is a local artist who started out making electronic music in the 1980s. Since then he has gone on to perform all over the world and is recognised as a pioneer of sound art and experimental music.
 </p>
 
+<ProfileImage src='/mark-min.jpg' alt='Mark Fell profile photo'/>
+
 <p>~</p>
 
 <p>
@@ -69,6 +79,10 @@
 <p>
     For this new collaboration Ed and Ryoko present two new sound installations, exploring their fascination with DJ playlists drawing from Ed’s work in the region as a prolific and groundbreaking DJ. Incorporating field recording, voice, music and electronic synthesis the listener is taken on a sound journey exploring ideas of place and identity. Ryoko, a sound artist based in Yorkshire, works with a particular sensitivity to space, environment and action, and has presented her work around the world. 
 </p>
+
+<ProfileImage src='/ed-min.jpg' alt='Ed Vector profile photo'/>
+
+<ProfileImage src='/ryoko-min.jpeg' alt='Ryoko profile photo'/>
 
 <p>
     Workshop 2pm
@@ -90,6 +104,8 @@
 <p>
     Treanor’s Polymutant Spikes uses digital synthesis and mathematical patterns to create sounds that move around the space. Visitors will be able to connect using their smartphones to change the sounds around them and interact with the patterns. Growing up on Broom Valley Road, Treanor is recognised as one of the UK’s most exciting and innovative producers, earning awards in DJ magazine, MixMag and Spotify, with guest spots on Radio 1. His latest album 'File Under UK Metaplasm' takes inspiration from his residency at the Nyege Nyege collective's Boutique Studio in Kampala in 2018. Incorporating the bass weight of his home town with the enigmatic energy of Tanzanian Singeli and Footwork. Here Treanor brings these influences to a new multichannel composition.
 </p>
+
+<ProfileImage src='/rian-min.jpg' alt='Rian Treanor profile photo'/>
 
 
 <style>
